ecafd62f9e95530d9a5bdd2dd4e8d76e556603e4
zhangyongbo <971953151@qq.com>
2026-09-15 20:23:29 +0800

    refactor(W32-D3): R2路由收口(/input /wizard /chat改redirect至选型中心页签,顶栏删重复对话入口,5处内部push由redirect自动承接)

--- web/src/App.vue
+++ web/src/App.vue
@@ -25,7 +25,6 @@ const active = computed(() => route.path)
           <el-menu-item index="/result">推荐结果</el-menu-item>
           <el-menu-item index="/compare">对比分析</el-menu-item>
           <el-menu-item index="/modify">部件修改</el-menu-item>
-          <el-menu-item index="/chat">智能体对话</el-menu-item>
         </el-menu>
       </div>
     </header>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -3,16 +3,16 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const routes = [
   { path: '/', redirect: '/select' },
   { path: '/select', name: 'select', component: () => import('../views/SelectCenterView.vue'), meta: { title: '选型中心' } },
-  { path: '/input', name: 'input', component: () => import('../views/InputView.vue'), meta: { title: '工况输入' } },
+  { path: '/input', redirect: '/select?tab=form' },
   { path: '/areas', name: 'areas', component: () => import('../views/AreasView.vue'), meta: { title: '矿区选择' } },
   { path: '/map', name: 'map', component: () => import('../views/MapView.vue'), meta: { title: '地图选区' } },
   { path: '/spectrum', name: 'spectrum', component: () => import('../views/SpectrumView.vue'), meta: { title: '架型谱系' } },
   { path: '/vendors', name: 'vendors', component: () => import('../views/VendorView.vue'), meta: { title: '厂商分布' } },
-  { path: '/wizard', name: 'wizard', component: () => import('../views/WizardView.vue'), meta: { title: '选型向导' } },
+  { path: '/wizard', redirect: '/select?tab=wizard' },
   { path: '/result', name: 'result', component: () => import('../views/ResultView.vue'), meta: { title: '推荐结果' } },
   { path: '/compare', name: 'compare', component: () => import('../views/CompareView.vue'), meta: { title: '支架对比' } },
   { path: '/modify', name: 'modify', component: () => import('../views/ModifyView.vue'), meta: { title: '部件修改' } },
-  { path: '/chat', name: 'chat', component: () => import('../views/ChatView.vue'), meta: { title: '智能体对话' } },
+  { path: '/chat', redirect: '/select?tab=chat' },
 ]
 
 const router = createRouter({
